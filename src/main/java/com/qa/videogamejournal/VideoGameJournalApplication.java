a4d003edb948f76002df622603283901ff18e89f
Cam-Black <cameronablack1998@gmail.com>
2022-07-07 22:11:24 +0100

    Initial Commit

---
+++ src/main/java/com/qa/videogamejournal/VideoGameJournalApplication.java
@@ -0,0 +1,13 @@
+package com.qa.videogamejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGameJournalApplication.class, args);
+    }
+
+}
