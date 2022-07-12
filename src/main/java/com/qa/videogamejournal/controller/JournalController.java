7f96206af3b5ba10f77e08ef91904d982e50c8c9
Cam-Black <cameronablack1998@gmail.com>
2022-07-12 17:29:03 +0100

    HP-20 #comment Added annotations for restcontroller and for spring tests.

--- src/main/java/com/qa/videogamejournal/controller/JournalController.java
+++ src/main/java/com/qa/videogamejournal/controller/JournalController.java
@@ -1,5 +1,8 @@
 package com.qa.videogamejournal.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JournalController {
 	private com.qa.videogamejournal.service.JournalService service;
 }
--- src/test/java/com/qa/videogamejournal/controller/JournalControllerIntegrationTest.java
+++ src/test/java/com/qa/videogamejournal/controller/JournalControllerIntegrationTest.java
@@ -1,4 +1,14 @@
 package com.qa.videogamejournal.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:journal-schema.sql", "classpath:journal-data.sql"},
+executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class JournalControllerIntegrationTest {
 }
