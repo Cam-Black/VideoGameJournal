e9246a5a3c1f17d66ed81c5bb0ade940e5b590a7
Cam-Black <cameronablack1998@gmail.com>
2022-07-19 15:39:56 +0100

    HP-32 #comment Stripped out unneeded code after using HTML to create card and populating the card with the response of the post request. Styled navbar to match index.html and added a separate style sheet for styling the card explicitly for the create.html page.

--- src/main/resources/static/front-end/js/create.js
+++ src/main/resources/static/front-end/js/create.js
@@ -1,50 +1,33 @@
 "use strict"
 
-const output = document.querySelector("#journalOutput");
+const cardTitle = document.querySelector(".card-title");
+const cardSubtitle = document.querySelector(".card-subtitle");
+const cardText =document.querySelector(".card-text");
 
 document.querySelector("#journal").addEventListener('submit', function(e) {
 	e.preventDefault();
 	
-	const data = {
+	const createBody = {
 		journalName: this.journalName.value,
 		gameName: this.gameName.value,
 		entry: this.entry.value
 	}
 	
-	axios.post("http://localhost:8080/journal/create", data)
+	axios.post("http://localhost:8080/journal/create", createBody)
 		.then(res => {
 			this.reset();
 			this.journalName.focus();
 			
-			const journalCard = document.createElement("div");
-			journalCard.className = "card";
-			
-			const journalBod = document.createElement("div");
-			journalBod.className = "card-body";
-			journalCard.appendChild(journalBod);
-			
-			const journalTitle = document.createElement("h5");
-			journalTitle.className = "card-title";
-			journalTitle.innerText = res.data.journalName;
-			journalBod.appendChild(journalTitle);
-			
-			const journalSubTitle = document.createElement("h6");
-			journalSubTitle.className = "card-subtitle";
-			journalSubTitle.innerText = res.data.gameName;
-			journalBod.appendChild(journalSubTitle);
-			
-			const journalText = document.createElement("p");
-			journalText.className = "card-text";
-			journalText.innerText = res.data.entry;
-			journalBod.appendChild(journalText);
-			
-			output.appendChild(journalCard);
-			
+			cardTitle.innerText = res.data.journalName;
+			cardSubtitle.innerText = res.data.gameName;
+			cardText.innerText = res.data.entry;
 		})
 		.catch(err => console.error(err));
 });
 
 document.querySelector("#journal").addEventListener('reset', function() {
-	output.innerHTML = "";
+	cardTitle.innerText = "";
+	cardSubtitle.innerText = "";
+	cardText.innerText = "";
 	this.journalName.focus();
 })
