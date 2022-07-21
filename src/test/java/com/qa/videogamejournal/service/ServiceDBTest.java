feddcb19d140f2d2cf575e8c533424b0d3572fcd
Cam-Black <cameronablack1998@gmail.com>
2022-07-21 21:20:55 +0100

    HP-44 #comment Created passing unit tests for getJournal and updateJournal.

--- src/test/java/com/qa/videogamejournal/service/ServiceDBTest.java
+++ src/test/java/com/qa/videogamejournal/service/ServiceDBTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class ServiceDBTest {
@@ -23,10 +24,10 @@ public class ServiceDBTest {
 	
 	@Test
 	void testCreateJournal() {
-		final Journal TEST_JOURNAL = new Journal(null, "Learning the Game", "Crusader Kings II",
+		final Journal TEST_JOURNAL = new Journal("Learning the Game", "Crusader Kings II",
 												 "Started the game as the ruler of Mumu in Old Ireland");
 		final Journal TEST_SAVED_JOURNAL = new Journal(1, "Learning the Game", "Crusader Kings II",
-													"Started the game as the ruler of Mumu in Old Ireland");
+													   "Started the game as the ruler of Mumu in Old Ireland");
 		Mockito.when(this.repo.save(TEST_JOURNAL)).thenReturn(TEST_SAVED_JOURNAL);
 		
 		Assertions.assertThat(this.service.createJournal(TEST_JOURNAL)).isEqualTo(TEST_SAVED_JOURNAL);
@@ -44,5 +45,40 @@ public class ServiceDBTest {
 		Assertions.assertThat(this.service.listJournals()).isEqualTo(TEST_JOURNAL_LIST);
 		
 		Mockito.verify(this.repo, Mockito.times(1)).findAll();
+	}
+	
+	@Test
+	void testGetJournal() {
+		final Journal TEST_JOURNAL = new Journal(1, "Going Dark", "Modern Warfare", "Infiltrated building at night");
+		
+		Mockito.when(this.repo.findById(1)).thenReturn(Optional.of(TEST_JOURNAL));
+		
+		Assertions.assertThat(this.service.getJournal(1)).isEqualTo(TEST_JOURNAL);
+		
+		Mockito.verify(this.repo, Mockito.times(1)).findById(1);
+	}
+	
+//	@Test
+//	void testDeleteJournal() {
+//		final Journal TEST_JOURNAL = new Journal(1, "My First Time", "Skyrim", "Finally got to the top of those " +
+//				"stairs... wait what is that thing?");
+//
+//		Mockito.when(this.repo.deleteById(1)).then();
+//
+//		Assertions.assertThat(this.service.deleteJournal(1)).isTrue();
+//
+//		Mockito.verify(this.repo, Mockito.times(1)).existsById(1);
+//	}
+	
+	@Test
+	void testUpdateJournal() {
+		final Journal TEST_JOURNAL = new Journal(1, "Name", "Game", "Entry");
+		final Journal UPDATED_JOURNAL = new Journal(1, "New Name", "New Game", "New Entry");
+		
+		Mockito.when(this.repo.save(TEST_JOURNAL)).thenReturn(UPDATED_JOURNAL);
+		
+		Assertions.assertThat(this.service.updateJournal(TEST_JOURNAL)).isEqualTo(UPDATED_JOURNAL);
+		
+		Mockito.verify(this.repo, Mockito.times(1)).save(TEST_JOURNAL);
 	}
 }
