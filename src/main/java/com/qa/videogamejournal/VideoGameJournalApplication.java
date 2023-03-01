8ec3893bf090e426f0cef4050b005af7b15c3ecd
Cam-Black <cameronablack1998@gmail.com>
2023-03-01 21:05:23 +0000

    Added Logger variable and utilising method for reading Password from console.

--- src/main/java/com/qa/videogamejournal/VideoGameJournalApplication.java
+++ src/main/java/com/qa/videogamejournal/VideoGameJournalApplication.java
@@ -1,13 +1,18 @@
 package com.qa.videogamejournal;
 
+import com.qa.videogamejournal.utils.IOUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VideoGameJournalApplication {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
+        String pass = IOUtil.getPassword();
+        System.setProperty("pass", pass);
         SpringApplication.run(VideoGameJournalApplication.class, args);
     }
-
 }
