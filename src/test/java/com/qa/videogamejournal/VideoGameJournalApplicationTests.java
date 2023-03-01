aa29d31bc8bf0c0cc6390a620593a19909049270
Cam-Black <cameronablack1998@gmail.com>
2023-03-01 21:05:40 +0000

    Added assertion to test.

--- src/test/java/com/qa/videogamejournal/VideoGameJournalApplicationTests.java
+++ src/test/java/com/qa/videogamejournal/VideoGameJournalApplicationTests.java
@@ -1,13 +1,20 @@
 package com.qa.videogamejournal;
 
+import com.qa.videogamejournal.controller.JournalController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class VideoGameJournalApplicationTests {
 
+    @Autowired
+    private JournalController controller;
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
-
 }
+
